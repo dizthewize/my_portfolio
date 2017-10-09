3f38525f029e179d633be6c94b70a59f5a3701b4
Unknown <martez_conner@outlook.com>
2017-10-09 01:09:58 -0700

    Added react files to project

---
+++ react-client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './app';
+
+const Root = document.getElementById('root');
+
+ReactDOM.render(, Root);
